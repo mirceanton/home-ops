8dc1fa17f554a90a6cc5cd1a61324d60d2ac11fa
Mircea-Pavel ANTON <contact@mirceanton.com>
2022-09-30 23:06:41 +0000

    Set project dir as env var

--- utils/scripts/automaton-backup.sh
+++ utils/scripts/automaton-backup.sh
@@ -5,6 +5,7 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+PROJECT_DIR="/workspace/amir-hlb"
 BACKUP_DIR="/var/log/backup_playbook"
 BACKUP_FILE="$BACKUP_DIR/$(date '+%Y-%m-%d-%H-%M-%S').log"
 
@@ -12,10 +13,10 @@ BACKUP_FILE="$BACKUP_DIR/$(date '+%Y-%m-%d-%H-%M-%S').log"
 sudo mkdir -p $BACKUP_DIR
 
 # Stop the services
-cd /workspace/amir-hlb && ansible-playbook infra/automaton/stop/main.yml >> $BACKUP_FILE
+cd $PROJECT_DIR && ansible-playbook infra/automaton/stop/main.yml >> $BACKUP_FILE
 
 # # Back up the data
-cd /workspace/amir-hlb && ansible-playbook infra/automaton/backup/main.yml >> $BACKUP_FILE
+cd $PROJECT_DIR && ansible-playbook infra/automaton/backup/main.yml >> $BACKUP_FILE
 
 # # Start all the services back up
-cd /workspace/amir-hlb && ansible-playbook infra/automaton/start/main.yml >> $BACKUP_FILE
+cd $PROJECT_DIR && ansible-playbook infra/automaton/start/main.yml >> $BACKUP_FILE
