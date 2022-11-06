77e0e1c613641c5da9e1a9aa77efa4a9cf6b6129
Mircea-Pavel ANTON <contact@mirceanton.com>
2022-11-06 17:02:13 +0000

    Update encryption/decryption scripts.

--- utils/scripts/decrypt.sh
+++ utils/scripts/decrypt.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-find inventory/host_vars -name "*.yml" -exec sops --decrypt --in-place --age $(cat $SOPS_AGE_KEY_FILE | grep -oP "public key: \K(.*)") {} \;
+find inventory/{host_vars,extra_vars} "*.yml" \
+  -exec sops --decrypt --in-place \
+  --age $(cat $SOPS_AGE_KEY_FILE | grep -oP "public key: \K(.*)") {} \;
--- utils/scripts/encrypt.sh
+++ utils/scripts/encrypt.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-find inventory/host_vars -name "*.yml" -exec sops --encrypt --in-place --age $(cat $SOPS_AGE_KEY_FILE | grep -oP "public key: \K(.*)") --encrypted-regex '^(ansible_host|ansible_user|ansible_ssh_pass)' {} \;
+find inventory/{host_vars,extra_vars} -name "*.yml" \
+  -exec sops --encrypt --in-place \
+  --age $(cat $SOPS_AGE_KEY_FILE | grep -oP "public key: \K(.*)") \
+  --encrypted-regex '(ansible_host|ansible_user|pass|token)' {} \;
