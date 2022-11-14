8b5be8e7b4e24f7f15d5bff63e6bd6d077d37ac6
Mircea-Pavel ANTON <contact@mirceanton.com>
2022-11-14 23:26:57 +0000

    Remove unused files.

--- utils/scripts/decrypt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-find inventory -name "*.yml" -type f \
-  -exec sops --decrypt --in-place \
-  --age $(cat $SOPS_AGE_KEY_FILE | grep -oP "public key: \K(.*)") {} \;
--- utils/scripts/encrypt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-find inventory/{host_vars,extra_vars} -name "*.yml" -type f\
-  -exec sops --encrypt --in-place \
-  --age $(cat $SOPS_AGE_KEY_FILE | grep -oP "public key: \K(.*)") \
-  --encrypted-regex '(ansible_host|ansible_user|pass|token)' {} \;
