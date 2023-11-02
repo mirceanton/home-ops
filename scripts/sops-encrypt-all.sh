8896d40a862428634cd42dcaab8f968f9b8a0103
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-11-02 14:32:51 +0000

    Colorize the output

--- scripts/sops-encrypt-all.sh
+++ scripts/sops-encrypt-all.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
 while IFS= read -r path; do
     path=$(echo "$path" | sed 's/\(\.sops\)/ /g')
     find . -regextype egrep -regex ".*/$path" -type f | while IFS= read -r file; do
@@ -9,19 +13,19 @@ while IFS= read -r path; do
             # Decrypt the encrypted version
             decrypted_temp=$(mktemp)
             sops --decrypt "$encrypted_file" > "$decrypted_temp"
-
+            
             # Compare the decrypted version with the file on disk
             if cmp -s "$file" "$decrypted_temp"; then
-                echo "No changes detected. Skipping encryption for file: $file"
+                echo -e "${GREEN}No changes detected. Skipping encryption for file: $file${NC}"
             else
-                echo "Changes detected. Re-encrypting file: $file"
+                echo -e "${RED}Changes detected. Re-encrypting file: $file${NC}"
                 sops --encrypt "$file" > "$encrypted_file"
             fi
-
+            
             rm "$decrypted_temp"
         else
             # No encrypted version exists, encrypt the file
-            echo "Encrypting file: $file"
+            echo -e "${RED}Encrypting file: $file${NC}"
             sops --encrypt "$file" > "$encrypted_file"
         fi
     done
