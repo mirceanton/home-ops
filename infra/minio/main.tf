0c2f8beaf78d4d860b50521b9a6180bc5e1e5aec
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-11-07 12:05:25 +0000

    Implement a sample tf script to create a bucket and SA

---
+++ infra/minio/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    minio = {
+      source = "aminueza/minio"
+      version = "2.0.0"
+    }
+  }
+}
+
+provider "minio" {
+  minio_server   = var.minio_server
+  minio_user     = var.minio_user
+  minio_password = var.minio_pass
+  minio_ssl = var.minio_ssl
+}
---
+++ infra/minio/vars.tf
@@ -0,0 +1,16 @@
+variable "minio_server" {
+  type = string
+}
+
+variable "minio_user" {
+  type = string
+}
+
+variable "minio_pass" {
+  type = string
+}
+
+variable "minio_ssl" {
+  type = bool
+  default = true
+}
