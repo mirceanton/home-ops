66e74323524c758b3d9e132a12caa8518d1133d4
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-11-10 21:13:27 +0000

    Create LAN DHCP Server

--- infra/mikrotik/network_lan.tf
+++ infra/mikrotik/network_lan.tf
@@ -15,4 +15,14 @@ resource "routeros_interface_bridge_port" "lan_bridge_port" {
   pvid      = "1"
 }
 
-
+resource "routeros_ip_pool" "lan_dhcp_pool" {
+  name   = "lan_dhcp_pool"
+  ranges = [
+    "192.168.69.10-192.168.69.199"
+  ]
+}
+resource "routeros_ip_dhcp_server" "lan_dhcp" {
+  address_pool = routeros_ip_pool.lan_dhcp_pool.name
+  interface    = routeros_interface_bridge.lan_bridge.name
+  name         = "lan_dhcp"
+}
