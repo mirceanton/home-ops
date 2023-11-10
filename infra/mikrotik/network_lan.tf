6033a8f29fd107db2be45ed724db8bfc04cbe91f
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-11-10 22:53:02 +0000

    Create dhcp server network

--- infra/mikrotik/network_lan.tf
+++ infra/mikrotik/network_lan.tf
@@ -16,13 +16,20 @@ resource "routeros_interface_bridge_port" "lan_bridge_port" {
 }
 
 resource "routeros_ip_pool" "lan_dhcp_pool" {
-  name   = "lan_dhcp_pool"
+  name = "lan_dhcp_pool"
   ranges = [
     "192.168.69.10-192.168.69.199"
   ]
 }
+resource "routeros_ip_dhcp_server_network" "lan_dhcp_network" {
+  address    = "192.168.69.0/24"
+  gateway    = split("/", routeros_ip_address.lan_address.address)[0]
+  dns_server = split("/", routeros_ip_address.lan_address.address)[0]
+  domain     = "home.mirceanton.com"
+}
 resource "routeros_ip_dhcp_server" "lan_dhcp" {
-  address_pool = routeros_ip_pool.lan_dhcp_pool.name
-  interface    = routeros_interface_bridge.lan_bridge.name
-  name         = "lan_dhcp"
+  address_pool     = routeros_ip_pool.lan_dhcp_pool.name
+  interface        = routeros_interface_bridge.lan_bridge.name
+  name             = "lan_dhcp"
+  client_mac_limit = 1
 }
