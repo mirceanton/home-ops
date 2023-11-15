ef69815c12d09923e52d06ccd120a33d8b8aa018
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-11-15 23:03:51 +0000

    Move objects to dedicated file

---
+++ infra/mikrotik/main.tf
@@ -0,0 +1,8 @@
+resource "routeros_system_identity" "identity" {
+  name = "MikroTik-RB5009"
+}
+
+resource "routeros_dns" "dns-server" {
+  allow_remote_requests = true
+  servers               = "1.1.1.1,8.8.8.8"
+}
--- infra/mikrotik/provider.tf
+++ infra/mikrotik/provider.tf
@@ -13,12 +13,3 @@ provider "routeros" {
   password = var.mikrotik_router_password
   insecure = var.mikrotik_router_insecure_skip_tls_verify
 }
-
-resource "routeros_system_identity" "identity" {
-  name = "MikroTik-RB5009"
-}
-
-resource "routeros_dns" "dns-server" {
-  allow_remote_requests = true
-  servers               = "1.1.1.1,8.8.8.8"
-}
