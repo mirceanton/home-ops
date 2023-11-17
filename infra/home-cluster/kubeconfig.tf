ce8ab95633f2822e580125d680340970a11e0ec7
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-11-17 15:32:58 +0000

    Update paths for configs

--- infra/home-cluster/kubeconfig.tf
+++ infra/home-cluster/kubeconfig.tf
@@ -2,11 +2,12 @@ data "talos_cluster_kubeconfig" "this" {
   depends_on           = [talos_machine_bootstrap.this]
   client_configuration = talos_machine_secrets.this.client_configuration
   node                 = [for k, v in var.node_data.controlplanes : k][0]
+  wait = true
 }
 
 resource "local_file" "kubeconfig" {
   content  = data.talos_cluster_kubeconfig.this.kubeconfig_raw
-  filename = "~/.kube/configs/${var.cluster_name}.yaml"
+  filename = pathexpand( "~/.kube/configs/${var.cluster_name}.yaml" )
 }
 
 output "kubeconfig" {
