475741e7684252e8c31a121369a54ddd3a2744f4
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-11-28 10:08:30 +0000

    Update variables

--- infra/cloudflare/variables.tf
+++ infra/cloudflare/variables.tf
@@ -3,7 +3,31 @@ variable "cf_email" {
   description = "value"
   sensitive = true
 }
+
 variable "cf_api_key" {
+  type = string
+  description = "value"
+  sensitive = true
+}
+
+variable "mirceanton_discord_txt" {
+  type = string
+  description = "value"
+  sensitive = true
+}
+
+variable "mirceanton_brevo_code" {
+  type = string
+  description = "value"
+  sensitive = true
+}
+variable "mirceanton_brevo_dkim" {
+  type = string
+  description = "value"
+  sensitive = true
+}
+
+variable "mirceanton_fwd_gmail" {
   type = string
   description = "value"
   sensitive = true
