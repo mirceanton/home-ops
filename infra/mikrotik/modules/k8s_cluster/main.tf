384c0b0f3e79d53dfc519331728314d676df987b
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-12-04 10:10:53 +0000

    Fix

--- infra/mikrotik/modules/k8s_cluster/main.tf
+++ infra/mikrotik/modules/k8s_cluster/main.tf
@@ -8,7 +8,7 @@ resource "routeros_ip_dhcp_server_lease" "cluster_nodes" {
 }
 
 resource "routeros_ip_dns_record" "cluster_service_lb_wildcards" {
-  for_each = var.service_lb.hostnames
+  for_each = toset(var.service_lb.hostnames)
 
   regexp  = ".*.${each.value}"
   address = var.service_lb.ip_address
@@ -16,7 +16,7 @@ resource "routeros_ip_dns_record" "cluster_service_lb_wildcards" {
 }
 
 resource "routeros_ip_dns_record" "home_cluster_k8s_vip" {
-  for_each = var.vip.hostnames
+  for_each = toset(var.vip.hostnames)
 
   name    = each.value
   address = var.vip.ip_address
