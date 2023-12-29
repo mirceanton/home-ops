1228d91712c0be4d6798f99942ca4f9513e97dca
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-12-29 16:39:41 +0000

    Split into dedicated files

--- terraform/minio/bucket_volsync.tf
+++ terraform/minio/bucket_volsync.tf
@@ -10,17 +10,3 @@ output "volsync_bucket_outputs" {
   value     = module.volsync_bucket
   sensitive = true
 }
-
-
-module "cnpg_bucket" {
-  source           = "./modules/minio_bucket"
-  bucket_name      = var.cnpg_bucket_name
-  owner_access_key = var.cnpg_bucket_access_key
-  owner_secret_key = var.cnpg_bucket_secret_key
-  is_public        = false
-}
-
-output "cnpg_bucket_outputs" {
-  value     = module.cnpg_bucket
-  sensitive = true
-}
