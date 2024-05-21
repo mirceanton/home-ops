5533498de4714747a20b61545d8f7ae2e0e0295e
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-05-21 19:41:10 +0000

    configure NAT on WAN bridge

--- terraform/mikrotik/network-wan.tf
+++ terraform/mikrotik/network-wan.tf
@@ -1,4 +1,17 @@
+## ================================================================================================
+## DHCP Client Config
+## ================================================================================================
 resource "routeros_ip_dhcp_client" "wan" {
   interface = routeros_interface_ethernet.wan.name
   comment = "WAN DHCP Client"
+}
+
+
+## ================================================================================================
+## NAT Configuration
+## ================================================================================================
+resource "routeros_ip_firewall_nat" "wan_default_nat" {
+  action        = "masquerade"
+  chain         = "srcnat"
+  out_interface = routeros_interface_bridge.wan.name
 }
