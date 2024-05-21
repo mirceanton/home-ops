911b0db2382389bfed1d7f20b46527a3a419f0df
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-05-21 19:27:59 +0000

    Set timezone

--- terraform/mikrotik/main.tf
+++ terraform/mikrotik/main.tf
@@ -6,3 +6,7 @@ resource "routeros_dns" "dns-server" {
   allow_remote_requests = true
   servers               = "1.1.1.1,8.8.8.8"
 }
+
+resource "routeros_system_clock" "timezone" {
+    time_zone_name = "Europe/Bucharest"
+}
