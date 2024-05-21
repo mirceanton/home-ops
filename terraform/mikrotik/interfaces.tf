71f10b69c4b0ba21ff8f41d2fd2b0fb55282e9ff
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-05-21 19:07:31 +0000

    disable unused interfaces

--- terraform/mikrotik/interfaces.tf
+++ terraform/mikrotik/interfaces.tf
@@ -35,9 +35,34 @@ resource "routeros_interface_ethernet" "home_assistant" {
   l2mtu        = 1514
 }
 
+resource "routeros_interface_ethernet" "ether6" {
+  factory_name = "ether6"
+  name         = "ether6"
+  comment      = "N/A"
+  disabled     = true
+  l2mtu        = 1514
+}
+
+resource "routeros_interface_ethernet" "ether7" {
+  factory_name = "ether7"
+  name         = "ether7"
+  comment      = "N/A"
+  disabled     = true
+  l2mtu        = 1514
+}
+
 resource "routeros_interface_ethernet" "management" {
   factory_name = "ether8"
   name         = "ether8"
   comment      = "Management"
   l2mtu        = 1514
+}
+
+resource "routeros_interface_ethernet" "sfp-sfpplus1" {
+  factory_name             = "sfp-sfpplus1"
+  name                     = "sfp-sfpplus1"
+  comment                  = "N/A"
+  l2mtu                    = 1514
+  disabled                 = true
+  sfp_shutdown_temperature = 90
 }
