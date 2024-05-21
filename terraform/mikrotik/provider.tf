0a833ef2b513acd6ef5e5f6c9030e8d477b9edfe
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-05-21 18:53:46 +0000

    Add provider configuration

---
+++ terraform/mikrotik/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    routeros = {
+      source  = "terraform-routeros/routeros"
+      version = "1.50.0"
+    }
+  }
+}
+
+provider "routeros" {
+  hosturl  = var.mikrotik_router_url
+  username = var.mikrotik_router_username
+  password = var.mikrotik_router_password
+  insecure = var.mikrotik_router_insecure_skip_tls_verify
+}
