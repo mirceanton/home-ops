8466c1e30bf5d3925196f1cd77e83c80e3ae3dcf
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-05-21 20:46:05 +0000

    add static dhcp leases for kube nodes

---
+++ terraform/mikrotik/network-management.tf
@@ -0,0 +1,80 @@
+## ================================================================================================
+## Bridge Network Configuration
+## ================================================================================================
+resource "routeros_interface_bridge" "management" {
+  name           = "brMAN"
+  vlan_filtering = true
+}
+resource "routeros_ip_address" "management" {
+  address   = "10.0.10.1/24"
+  interface = routeros_interface_bridge.management.name
+  network   = "10.0.10.0"
+}
+
+
+## ================================================================================================
+## DHCP Server Config
+## ================================================================================================
+resource "routeros_ip_dhcp_server" "management" {
+  address_pool       = routeros_ip_pool.management.name
+  interface          = routeros_interface_bridge.management.name
+  name               = "MAN-dhcp"
+  client_mac_limit   = 1
+  conflict_detection = true
+}
+resource "routeros_ip_dhcp_server_network" "management" {
+  address      = "10.0.10.0/24"
+  caps_manager = "10.0.10.1"
+  comment      = "Management DHCP Server Network"
+  dns_server   = "10.0.10.1"
+  domain       = "management.mirceanton.com"
+  gateway      = "10.0.10.1"
+}
+resource "routeros_ip_pool" "management" {
+  name = "management-dhcp-pool"
+  ranges = [
+    "10.0.10.190-10.0.10.199"
+  ]
+}
+resource "routeros_ip_dhcp_server_lease" "cisco_sg350" {
+  address     = "10.0.10.2"
+  mac_address = "00:EE:AB:28:1C:81"
+  server      = routeros_ip_dhcp_server.management.name
+  comment     = "Management Switch - Cisco SG350-10"
+}
+resource "routeros_ip_dhcp_server_lease" "odroid_c4" {
+  address     = "10.0.10.69"
+  mac_address = "00:1E:06:48:6A:28"
+  server      = routeros_ip_dhcp_server.management.name
+  comment     = "Odroid C4"
+}
+resource "routeros_ip_dhcp_server_lease" "kube_01" {
+  address     = "10.0.10.51"
+  mac_address = "74:56:3C:9E:BF:1A"
+  server      = routeros_ip_dhcp_server.management.name
+  comment     = "Kube 01"
+}
+resource "routeros_ip_dhcp_server_lease" "kube_02" {
+  address     = "10.0.10.52"
+  mac_address = "74:56:3C:B2:E5:A8"
+  server      = routeros_ip_dhcp_server.management.name
+  comment     = "Kube 02"
+}
+resource "routeros_ip_dhcp_server_lease" "kube_03" {
+  address     = "10.0.10.53"
+  mac_address = "74:56:3C:99:5B:CE"
+  server      = routeros_ip_dhcp_server.management.name
+  comment     = "Kube 03"
+}
+
+
+
+## ================================================================================================
+## Bridge Ports
+## ================================================================================================
+resource "routeros_interface_bridge_port" "management" {
+  bridge    = routeros_interface_bridge.management.name
+  interface = routeros_interface_ethernet.management.name
+  comment   = routeros_interface_ethernet.management.comment
+  pvid      = "1"
+}
