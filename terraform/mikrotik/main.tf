71aeadcaac14162669fad36a5368fa4d2b9dfd15
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-05-21 18:54:25 +0000

    Set dns server

--- terraform/mikrotik/main.tf
+++ terraform/mikrotik/main.tf
@@ -1,3 +1,8 @@
 resource "routeros_system_identity" "rb5009" {
   name = "MikroTik-RB5009"
 }
+
+resource "routeros_dns" "dns-server" {
+  allow_remote_requests = true
+  servers               = "1.1.1.1,8.8.8.8"
+}
