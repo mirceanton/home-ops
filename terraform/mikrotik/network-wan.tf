2991b323ff46ff40242d006cb0d1d0911a45c308
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-05-21 19:41:30 +0000

    configure WAN bridge :)

--- terraform/mikrotik/network-wan.tf
+++ terraform/mikrotik/network-wan.tf
@@ -1,4 +1,19 @@
 ## ================================================================================================
+## Bridge Network Configuration
+## ================================================================================================
+resource "routeros_interface_bridge" "wan" {
+  name           = "brWAN"
+  vlan_filtering = false
+}
+
+resource "routeros_interface_bridge_port" "wan" {
+  bridge    = routeros_interface_bridge.wan.name
+  interface = routeros_interface_ethernet.wan.name
+  pvid      = "1"
+}
+
+
+## ================================================================================================
 ## DHCP Client Config
 ## ================================================================================================
 resource "routeros_ip_dhcp_client" "wan" {
