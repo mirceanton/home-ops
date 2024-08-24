3e724a2fa89b11e6852e6d4bf28e1e736f609ad2
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-08-24 14:52:10 +0000

    configure winbox server

--- terraform/mikrotik/system.tf
+++ terraform/mikrotik/system.tf
@@ -12,3 +12,7 @@ resource "routeros_dns" "dns-server" {
 resource "routeros_system_clock" "timezone" {
   time_zone_name = "Europe/Bucharest"
 }
+
+resource "routeros_tool_mac_server_winbox" "mac_server_winbox" {
+  allowed_interface_list = "all"
+}
