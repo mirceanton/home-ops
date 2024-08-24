c4af784733c0036cfd799c6fabffde34f96b7f8c
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-08-24 14:51:34 +0000

    configure SSL services

--- terraform/mikrotik/ip-services.tf
+++ terraform/mikrotik/ip-services.tf
@@ -1,6 +1,7 @@
 locals {
+  tls_service     = { "api-ssl" = 8729, "www-ssl" = 443 }
   disable_service = { "api" = 8728, "ftp" = 21, "telnet" = 23, "www" = 80, "ssh" = 22 }
-  enable_service  = { "winbox" = 8291, "api-ssl" = 8729, "www-ssl" = 443 }
+  enable_service  = { "winbox" = 8291 }
 }
 
 resource "routeros_ip_service" "disabled" {
@@ -54,4 +55,13 @@ resource "routeros_system_certificate" "webfig" {
   sign {
     ca = routeros_system_certificate.local-root-ca-cert.name
   }
+}
+
+resource "routeros_ip_service" "ssl" {
+  for_each    = local.tls_service
+  numbers     = each.key
+  port        = each.value
+  tls_version = "only-1.2"
+  disabled    = false
+  certificate = routeros_system_certificate.webfig.name
 }
