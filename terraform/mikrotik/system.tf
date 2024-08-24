ebb6ae5ea26946d02a6c01aedbf85f19d21b8962
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-08-24 11:51:26 +0000

    set default DNS servers

--- terraform/mikrotik/system.tf
+++ terraform/mikrotik/system.tf
@@ -2,6 +2,13 @@ resource "routeros_system_identity" "rb5009" {
   name = "MikroTik-RB5009"
 }
 
+resource "routeros_dns" "dns-server" {
+  allow_remote_requests = true
+  servers = [
+    "1.1.1.1", "8.8.8.8",
+  ]
+}
+
 resource "routeros_system_clock" "timezone" {
   time_zone_name = "Europe/Bucharest"
 }
