87d530e4a3d67882301a94e29b90c544c915485c
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-08-25 14:33:28 +0000

    bridge ports for cluster network

--- terraform/mikrotik/network_cluster.tf
+++ terraform/mikrotik/network_cluster.tf
@@ -10,3 +10,14 @@ resource "routeros_ip_address" "cluster" {
   interface = routeros_interface_bridge.cluster.name
   network   = "10.0.10.0"
 }
+
+
+## ================================================================================================
+## Bridge Ports
+## ================================================================================================
+resource "routeros_interface_bridge_port" "cluster_switch" {
+  bridge    = routeros_interface_bridge.cluster.name
+  interface = routeros_interface_ethernet.cluster.name
+  comment   = routeros_interface_ethernet.cluster.comment
+  pvid      = "1"
+}
