b4c6fba36c934bdc9183dcecc19c75e5f11647fe
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-08-25 14:34:16 +0000

    add DHCP server for cluster network

--- terraform/mikrotik/network_cluster.tf
+++ terraform/mikrotik/network_cluster.tf
@@ -21,3 +21,28 @@ resource "routeros_interface_bridge_port" "cluster_switch" {
   comment   = routeros_interface_ethernet.cluster.comment
   pvid      = "1"
 }
+
+
+## ================================================================================================
+## DHCP Server Config
+## ================================================================================================
+resource "routeros_ip_pool" "cluster" {
+  name    = "cluster-dhcp-pool"
+  comment = "Cluster DHCP Pool"
+  ranges  = ["10.0.10.190-10.0.10.199"]
+}
+resource "routeros_ip_dhcp_server_network" "cluster" {
+  comment    = "Cluster DHCP Network"
+  domain     = "cluster.${local.internal_domain}"
+  address    = "10.0.10.0/24"
+  gateway    = "10.0.10.1"
+  dns_server = ["10.0.10.1"]
+}
+resource "routeros_ip_dhcp_server" "cluster" {
+  name               = "cluster-dhcp"
+  comment            = "Cluster DHCP Server"
+  address_pool       = routeros_ip_pool.cluster.name
+  interface          = routeros_interface_bridge.cluster.name
+  client_mac_limit   = 1
+  conflict_detection = false
+}
