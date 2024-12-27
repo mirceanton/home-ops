b66ac738d2a9b1da254194f3839cd4669c430f95
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-12-27 13:59:50 +0000

    feat: Configure WAN to use PPPoE instead of DHCP

--- terraform/mikrotik/network_wan.tf
+++ terraform/mikrotik/network_wan.tf
@@ -10,7 +10,6 @@ resource "routeros_interface_bridge" "wan" {
 ## ================================================================================================
 ## Bridge Ports
 ## ================================================================================================
-
 resource "routeros_interface_bridge_port" "wan_uplink" {
   bridge    = routeros_interface_bridge.wan.name
   interface = routeros_interface_ethernet.wan.name
@@ -18,10 +17,14 @@ resource "routeros_interface_bridge_port" "wan_uplink" {
   pvid      = "1"
 }
 
+
 ## ================================================================================================
-## DHCP Client Config
+## PPPoE Client Config
 ## ================================================================================================
-resource "routeros_ip_dhcp_client" "wan" {
-  interface = routeros_interface_bridge.wan.name
-  comment   = "WAN DHCP Client"
+resource "routeros_interface_pppoe_client" "wan" {
+  disabled  = false
+  interface = routeros_interface_ethernet.wan.name
+  name      = "PPPoE Digi"
+  user      = var.pppoe_client_user
+  password  = var.pppoe_client_pass
 }
