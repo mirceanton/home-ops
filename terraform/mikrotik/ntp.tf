6ceaafa44aa6a47978e4f4615ee601daebd43315
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-12-27 19:28:26 +0000

    configure ntp per device

--- terraform/mikrotik/ntp.tf
+++ terraform/mikrotik/ntp.tf
@@ -1,4 +1,6 @@
-resource "routeros_system_ntp_server" "ntp" {
+resource "routeros_system_ntp_server" "rb5009" {
+  provider = routeros.rb5009
+
   enabled             = true
   broadcast           = true
   multicast           = true
@@ -7,8 +9,24 @@ resource "routeros_system_ntp_server" "ntp" {
   local_clock_stratum = 3
 }
 
-resource "routeros_system_ntp_client" "ntp" {
+resource "routeros_system_ntp_client" "rb5009" {
+  provider = routeros.rb5009
+
   enabled = true
   mode    = "unicast"
   servers = ["89.36.19.21"]
+}
+resource "routeros_system_ntp_client" "crs317" {
+  provider = routeros.crs317
+
+  enabled = true
+  mode    = "unicast"
+  servers = ["192.168.88.1"]
+}
+resource "routeros_system_ntp_client" "crs326" {
+  provider = routeros.crs326
+
+  enabled = true
+  mode    = "unicast"
+  servers = ["192.168.88.1"]
 }
