0ba4e805b57df72f1a6ce171af99e2de64cef030
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-12-27 19:31:16 +0000

    configure system settings for each device

--- terraform/mikrotik/system.tf
+++ terraform/mikrotik/system.tf
@@ -1,24 +1,77 @@
 resource "routeros_system_identity" "rb5009" {
-  name = "MikroTik-RB5009"
+  provider = routeros.rb5009
+  name     = "Router"
+}
+resource "routeros_system_identity" "crs326" {
+  provider = routeros.crs326
+  name     = "Rack Slow"
+}
+resource "routeros_system_identity" "crs317" {
+  provider = routeros.crs317
+  name     = "Rack Fast"
 }
 
+
+resource "routeros_system_clock" "rb5009" {
+  provider       = routeros.rb5009
+  time_zone_name = "Europe/Bucharest"
+}
+resource "routeros_system_clock" "crs326" {
+  provider       = routeros.crs326
+  time_zone_name = "Europe/Bucharest"
+}
+resource "routeros_system_clock" "crs317" {
+  provider       = routeros.crs317
+  time_zone_name = "Europe/Bucharest"
+}
+
+
 resource "routeros_dns" "dns-server" {
+  provider = routeros.rb5009
+
   allow_remote_requests = true
   servers = [
     "1.1.1.1", "8.8.8.8",
   ]
 }
 
-resource "routeros_system_clock" "timezone" {
-  time_zone_name = "Europe/Bucharest"
+resource "routeros_tool_mac_server_winbox" "rb5009" {
+  provider               = routeros.rb5009
+  allowed_interface_list = "all"
+}
+resource "routeros_tool_mac_server" "rb5009" {
+  provider               = routeros.rb5009
+  allowed_interface_list = "all"
+}
+resource "routeros_tool_bandwidth_server" "rb5009" {
+  provider = routeros.rb5009
+  enabled  = false
 }
 
-resource "routeros_tool_mac_server_winbox" "mac_server_winbox" {
+
+resource "routeros_tool_mac_server_winbox" "crs317" {
+  provider               = routeros.crs317
   allowed_interface_list = "all"
 }
-resource "routeros_tool_mac_server" "mac_server" {
+resource "routeros_tool_mac_server" "crs317" {
+  provider               = routeros.crs317
   allowed_interface_list = "all"
 }
-resource "routeros_tool_bandwidth_server" "bandwidth_server" {
-  enabled = false
+resource "routeros_tool_bandwidth_server" "crs317" {
+  provider = routeros.crs317
+  enabled  = false
+}
+
+
+resource "routeros_tool_mac_server_winbox" "crs326" {
+  provider               = routeros.crs326
+  allowed_interface_list = "all"
+}
+resource "routeros_tool_mac_server" "crs326" {
+  provider               = routeros.crs326
+  allowed_interface_list = "all"
+}
+resource "routeros_tool_bandwidth_server" "crs326" {
+  provider = routeros.crs326
+  enabled  = false
 }
