2cbbd0de4508be09c14e15cba24180b65540630f
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-12-27 13:58:36 +0000

    chore: Update bitwarden provider from 0.8.0 to 0.12.1

--- terraform/mikrotik/_provider.tf
+++ terraform/mikrotik/_provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
     bitwarden = {
       source  = "maxlaverse/bitwarden"
-      version = "0.8.0"
+      version = "0.12.1"
     }
     random = {
       source  = "hashicorp/random"
