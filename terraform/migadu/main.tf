0a6a2da32556165ab480f7c5205f2014b6ce10c6
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-12-28 18:02:19 +0000

    Use mailbox module

---
+++ terraform/migadu/main.tf
@@ -0,0 +1,51 @@
+locals {
+  domain = "mirceanton.com"
+}
+
+module "personal_mailbox" {
+  source                  = "./modules/mailbox"
+  name                    = "Personal"
+  domain_name             = local.domain
+  may_receive             = true
+  may_send                = true
+  may_access_imap         = true
+  local_part              = "mircea"
+  password_recovery_email = var.migadu_api_email
+  aliases                 = ["me", "personal"]
+}
+
+module "business_mailbox" {
+  source                  = "./modules/mailbox"
+  name                    = "Business"
+  domain_name             = local.domain
+  may_receive             = true
+  may_send                = true
+  may_access_imap         = true
+  local_part              = "business"
+  password_recovery_email = var.migadu_api_email
+  aliases                 = ["contact"]
+}
+
+module "noreply_mailbox" {
+  source                  = "./modules/mailbox"
+  name                    = "NoReply"
+  domain_name             = local.domain
+  may_receive             = false
+  may_send                = true
+  may_access_imap         = false
+  local_part              = "noreply"
+  password_recovery_email = var.migadu_api_email
+  aliases                 = []
+}
+
+module "spam_mailbox" {
+  source                  = "./modules/mailbox"
+  name                    = "Spam"
+  domain_name             = local.domain
+  may_receive             = true
+  may_send                = false
+  may_access_imap         = true
+  local_part              = "spam"
+  password_recovery_email = var.migadu_api_email
+  aliases                 = []
+}
