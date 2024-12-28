8eef39c62cadf7b96719b595a3432b71e2deba7f
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-12-28 13:33:39 +0000

    refactor

--- terraform/cloudflare/mirceanton.tf
+++ terraform/cloudflare/mirceanton.tf
@@ -3,193 +3,116 @@ data "cloudflare_zone" "mirceanton" {
 }
 
 # =================================================================================================
-# Discord Verification
+# Various TXT Verifications
 # =================================================================================================
-resource "cloudflare_record" "mirceanton_discord_verification" {
+resource "cloudflare_record" "mirceanton_TXT_verifications" {
+  for_each = {
+    "Discord Verification" = { name = "_discord.com", content = "\"${var.discord_verification_mirceanton}\"" },
+    "Google Verification"  = { name = "mirceanton.com", content = "\"${var.google_verification_mirceanton}\"" }
+  }
+
   zone_id = data.cloudflare_zone.mirceanton.id
-  name    = "_discord"
-  comment = "Terraform: Discord Verification"
   type    = "TXT"
   proxied = false
-  content = "\"${var.discord_verification_mirceanton}\""
-}
 
-
-# =================================================================================================
-# Google Verification
-# =================================================================================================
-resource "cloudflare_record" "mirceanton_google_verification" {
-  zone_id = data.cloudflare_zone.mirceanton.id
-  name    = "mirceanton.com"
-  comment = "Terraform: Google Verification"
-  type    = "TXT"
-  proxied = false
-  content = "\"${var.google_verification_mirceanton}\""
+  name    = each.value.name
+  comment = "Terraform: ${each.key}"
+  content = each.value.content
 }
 
 # =================================================================================================
 # GitHub Pages
 # =================================================================================================
 resource "cloudflare_record" "mirceanton_github_pages" {
-  for_each = toset(["185.199.111.153", "185.199.110.153", "185.199.109.153", "185.199.108.153"])
+  for_each = {
+    "WWW" = { name = "www", type = "CNAME", content = "mirceanton.github.io" },
+
+    "Verification" = { name = "_github-pages-challenge-mirceanton", type = "TXT", content = "\"${var.github_verification_mirceanton}\"" }
+
+    "A1" = { name = "mirceanton.com", type = "A", content = "185.199.111.153" }
+    "A2" = { name = "mirceanton.com", type = "A", content = "185.199.110.153" }
+    "A3" = { name = "mirceanton.com", type = "A", content = "185.199.109.153" }
+    "A4" = { name = "mirceanton.com", type = "A", content = "185.199.108.153" }
+  }
 
   zone_id = data.cloudflare_zone.mirceanton.id
-  name    = "mirceanton.com"
-  comment = "Terraform: GitHub Pages"
-  type    = "A"
   proxied = false
-  content = each.value
-}
-resource "cloudflare_record" "mirceanton_github_pages_verification" {
-  zone_id = data.cloudflare_zone.mirceanton.id
-  name    = "_github-pages-challenge-mirceanton"
-  comment = "Terraform: GitHub Pages"
-  type    = "TXT"
-  proxied = false
-  content = "\"${var.github_verification_mirceanton}\""
-}
-resource "cloudflare_record" "mirceanton_github_pages_www" {
-  zone_id = data.cloudflare_zone.mirceanton.id
-  name    = "www"
-  content = "mirceanton.github.io"
-  comment = "Terraform: GitHub Pages"
-  type    = "CNAME"
-  proxied = false
+
+  type    = each.value.type
+  name    = each.value.name
+  content = each.value.content
+  comment = "Terraform: ${each.key}"
 }
 
 # =================================================================================================
 # Migadu
 # =================================================================================================
-resource "cloudflare_record" "mirceanton_migadu_verification" {
+resource "cloudflare_record" "mirceanton_migadu_TXT" {
+  for_each = {
+    "Verification" = { name = "mirceanton.com", content = "\"${var.migadu_verification_mirceanton}\"" }
+    "SPF"          = { name = "mirceanton.com", content = "\"v=spf1 include:spf.migadu.com -all\"" }
+    "DMARC"        = { name = "_dmarc", content = "\"v=DMARC1; p=quarantine;\"" }
+  }
   zone_id = data.cloudflare_zone.mirceanton.id
-  name    = "mirceanton.com"
-  comment = "Terraform: Migadu Verification"
   type    = "TXT"
   proxied = false
-  content = "\"${var.migadu_verification_mirceanton}\""
-}
-resource "cloudflare_record" "mirceanton_migadu_spf" {
-  zone_id = data.cloudflare_zone.mirceanton.id
-  name    = "mirceanton.com"
-  comment = "Terraform: Migadu SPF"
-  type    = "TXT"
-  proxied = false
-  content = "\"v=spf1 include:spf.migadu.com -all\""
-}
-resource "cloudflare_record" "mirceanton_migadu_dmarc" {
-  zone_id = data.cloudflare_zone.mirceanton.id
-  name    = "_dmarc"
-  comment = "Terraform: Migadu DMARC"
-  type    = "TXT"
-  proxied = false
-  content = "\"v=DMARC1; p=quarantine;\""
-}
-resource "cloudflare_record" "mirceanton_migadu_dkim" {
-  for_each = toset(["key1", "key2", "key3"])
 
+  name    = each.value.name
+  content = each.value.content
+  comment = "Terraform: Migadu ${each.key}"
+}
+
+resource "cloudflare_record" "mirceanton_migadu_CNAME" {
+  for_each = {
+    "DKIM Primary"           = { name = "key1._domainkey", content = "key1.mirceanton.com._domainkey.migadu.com" }
+    "DKIM Secondary"         = { name = "key2._domainkey", content = "key2.mirceanton.com._domainkey.migadu.com" }
+    "DKIM Tertiary"          = { name = "key3._domainkey", content = "key3.mirceanton.com._domainkey.migadu.com" }
+    "Autoconfig Thunderbird" = { name = "autoconfig", content = "autoconfig.migadu.com" }
+  }
   zone_id = data.cloudflare_zone.mirceanton.id
-  name    = "${each.value}._domainkey"
-  content = "${each.value}.mirceanton.com._domainkey.migadu.com"
-  comment = "Terraform: Migadu DKIM"
   type    = "CNAME"
   proxied = false
+
+  name    = each.value.name
+  content = each.value.content
+  comment = "Terraform: Migadu ${each.key}"
 }
 
-resource "cloudflare_record" "mirceanton_migadu_autoconfig_thunderbird" {
-  zone_id = data.cloudflare_zone.mirceanton.id
-  name    = "autoconfig"
-  content = "autoconfig.migadu.com"
-  comment = "Terraform: Migadu Autoconfig for Thunderbird"
-  type    = "CNAME"
-  proxied = false
-}
-resource "cloudflare_record" "mirceanton_migadu_autoconfig_outlook" {
-  zone_id = data.cloudflare_zone.mirceanton.id
-  name    = "_autodiscover._tcp"
-  comment = "Terraform: Migadu Autoconfig for Outlook"
-  proxied = false
-  type    = "SRV"
-  data {
-    priority = 0
-    weight   = 1
-    port     = 443
-    target   = "autodiscover.migadu.com"
+resource "cloudflare_record" "mirceanton_migadu_SRV" {
+  for_each = {
+    "Autoconfig for Outlook" = { domain = "_autodiscover._tcp", priority = 0, weight = 1, port = 443, target = "autodiscover.migadu.com" }
+    "Autoconfig SMTP"        = { domain = "_submissions._tcp", priority = 0, weight = 1, port = 465, target = "smtp.migadu.com" }
+    "Autoconfig IMAP"        = { domain = "_imaps._tcp", priority = 0, weight = 1, port = 993, target = "imap.migadu.com" }
+    "Autoconfig POP3"        = { domain = "_pop3s._tcp", priority = 0, weight = 1, port = 995, target = "pop3.migadu.com" }
   }
-}
-resource "cloudflare_record" "mirceanton_migadu_autoconfig_imap" {
   zone_id = data.cloudflare_zone.mirceanton.id
-  name    = "_imaps._tcp"
-  comment = "Terraform: Migadu Autoconfig IMAP"
-  proxied = false
   type    = "SRV"
-  data {
-    priority = 0
-    weight   = 1
-    port     = 993
-    target   = "imap.migadu.com"
-  }
-}
-resource "cloudflare_record" "mirceanton_migadu_autoconfig_pop" {
-  zone_id = data.cloudflare_zone.mirceanton.id
-  name    = "_pop3s._tcp"
-  comment = "Terraform: Migadu Autoconfig POP3"
   proxied = false
-  type    = "SRV"
+
+  name = each.value.domain
   data {
-    priority = 0
-    weight   = 1
-    port     = 995
-    target   = "pop.migadu.com"
+    priority = each.value.priority
+    weight   = each.value.weight
+    port     = each.value.port
+    target   = each.value.target
   }
-}
-resource "cloudflare_record" "mirceanton_migadu_autoconfig_smtp" {
-  zone_id = data.cloudflare_zone.mirceanton.id
-  name    = "_submissions._tcp"
-  comment = "Terraform: Migadu Autoconfig SMTP"
-  proxied = false
-  type    = "SRV"
-  data {
-    priority = 0
-    weight   = 1
-    port     = 465
-    target   = "smtp.migadu.com"
-  }
+  comment = "Terraform: Migadu ${each.key}"
 }
 
+resource "cloudflare_record" "mirceanton_migadu_MX" {
+  for_each = {
+    "Primary MX Host"                          = { priority = 10, content = "aspmx1.migadu.com", name = "mirceanton.com" }
+    "Secondary MX Host"                        = { priority = 20, content = "aspmx2.migadu.com", name = "mirceanton.com" }
+    "Primary MX Host (Subdomain Addressing)"   = { priority = 10, content = "aspmx1.migadu.com", name = "*" }
+    "Secondary MX Host (Subdomain Addressing)" = { priority = 20, content = "aspmx2.migadu.com", name = "*" }
+  }
 
-resource "cloudflare_record" "mirceanton_migadu_mx_domain_primary" {
-  zone_id  = data.cloudflare_zone.mirceanton.id
-  name     = "mirceanton.com"
-  content  = "aspmx1.migadu.com"
-  comment  = "Terraform: Migadu Primary MX Host (Subdomain Addressing)"
-  type     = "MX"
-  priority = 10
-  proxied  = false
-}
-resource "cloudflare_record" "mirceanton_migadu_mx_domain_secondary" {
-  zone_id  = data.cloudflare_zone.mirceanton.id
-  name     = "mirceanton.com"
-  content  = "aspmx2.migadu.com"
-  comment  = "Terraform: Migadu Secondary MX Host (Subdomain Addressing)"
-  type     = "MX"
-  priority = 20
-  proxied  = false
-}
-resource "cloudflare_record" "mirceanton_migadu_mx_subdomain_primary" {
-  zone_id  = data.cloudflare_zone.mirceanton.id
-  name     = "*"
-  content  = "aspmx1.migadu.com"
-  comment  = "Terraform: Migadu Primary MX Host (Subdomain Addressing)"
-  type     = "MX"
-  priority = 10
-  proxied  = false
-}
-resource "cloudflare_record" "mirceanton_migadu_mx_subdomain_secondary" {
-  zone_id  = data.cloudflare_zone.mirceanton.id
-  name     = "*"
-  content  = "aspmx2.migadu.com"
-  comment  = "Terraform: Migadu Secondary MX Host (Subdomain Addressing)"
-  type     = "MX"
-  priority = 20
-  proxied  = false
+  zone_id = data.cloudflare_zone.mirceanton.id
+  type    = "MX"
+  proxied = false
+
+  name     = each.value.name
+  priority = each.value.priority
+  content  = each.value.content
+  comment  = "Terraform: Migadu ${each.key}"
 }
