29fa5e00056b4e0dca8d156f2263829ec64a2bea
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-12-29 16:09:58 +0000

    Initialize module per device

---
+++ terraform/mikrotik/main.tf
@@ -0,0 +1,23 @@
+module "rb5009" {
+  source            = "./modules/rb5009"
+  mikrotik_host_url = "https://192.168.88.1"
+  mikrotik_username = var.mikrotik_username
+  mikrotik_password = var.mikrotik_password
+  mikrotik_insecure = true
+}
+
+module "crs317" {
+  source            = "./modules/crs317"
+  mikrotik_host_url = "https://192.168.88.2"
+  mikrotik_username = var.mikrotik_username
+  mikrotik_password = var.mikrotik_password
+  mikrotik_insecure = true
+}
+
+module "crs326" {
+  source            = "./modules/crs326"
+  mikrotik_host_url = "https://192.168.88.3"
+  mikrotik_username = var.mikrotik_username
+  mikrotik_password = var.mikrotik_password
+  mikrotik_insecure = true
+}
