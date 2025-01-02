5cb7a12eedb23e3a0352234cd2e7687dfd0a7feb
Mircea-Pavel ANTON <contact@mirceanton.com>
2025-01-02 23:36:55 +0200

    Configure native VLANs for the ports on the router?

--- terraform/mikrotik/modules/rb5009/bridge.tf
+++ terraform/mikrotik/modules/rb5009/bridge.tf
@@ -8,3 +8,27 @@ resource "routeros_interface_bridge" "bridge" {
   disabled       = false
   vlan_filtering = true
 }
+
+
+# =================================================================================================
+# Bridge Ports
+# https://registry.terraform.io/providers/terraform-routeros/routeros/latest/docs/resources/interfa
+# =================================================================================================ce_bridge_port
+resource "routeros_interface_bridge_port" "living_room" {
+  bridge    = routeros_interface_bridge.bridge.name
+  interface = routeros_interface_ethernet.living_room.name
+  comment   = routeros_interface_ethernet.living_room.comment
+  pvid      = routeros_interface_vlan.servers.vlan_id
+}
+resource "routeros_interface_bridge_port" "sploinkhole" {
+  bridge    = routeros_interface_bridge.bridge.name
+  interface = routeros_interface_ethernet.sploinkhole.name
+  comment   = routeros_interface_ethernet.sploinkhole.comment
+  pvid      = routeros_interface_vlan.trusted.vlan_id
+}
+resource "routeros_interface_bridge_port" "access_point" {
+  bridge    = routeros_interface_bridge.bridge.name
+  interface = routeros_interface_ethernet.access_point.name
+  comment   = routeros_interface_ethernet.access_point.comment
+  pvid      = routeros_interface_vlan.servers.vlan_id
+}
