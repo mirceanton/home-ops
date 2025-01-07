a2479c8e4e4f4da70a3408683f1a0426cfab5fbf
Mircea-Pavel ANTON <contact@mirceanton.com>
2025-01-07 07:12:08 +0000

    add laggs to bridge

--- terraform/mikrotik/modules/crs317/bridge.tf
+++ terraform/mikrotik/modules/crs317/bridge.tf
@@ -8,3 +8,52 @@ resource "routeros_interface_bridge" "bridge" {
   disabled       = false
   vlan_filtering = true
 }
+
+
+# =================================================================================================
+# Bridge Ports
+# https://registry.terraform.io/providers/terraform-routeros/routeros/latest/docs/resources/interfa
+# =================================================================================================ce_bridge_port
+resource "routeros_interface_bridge_port" "nas" {
+  bridge    = routeros_interface_bridge.bridge.name
+  interface = routeros_interface_bonding.nas.name
+  comment   = routeros_interface_bonding.nas.comment
+  pvid      = "1"
+}
+resource "routeros_interface_bridge_port" "kube01" {
+  bridge    = routeros_interface_bridge.bridge.name
+  interface = routeros_interface_bonding.kube01.name
+  comment   = routeros_interface_bonding.kube01.comment
+  pvid      = "1"
+}
+resource "routeros_interface_bridge_port" "kube02" {
+  bridge    = routeros_interface_bridge.bridge.name
+  interface = routeros_interface_bonding.kube02.name
+  comment   = routeros_interface_bonding.kube02.comment
+  pvid      = "1"
+}
+resource "routeros_interface_bridge_port" "kube03" {
+  bridge    = routeros_interface_bridge.bridge.name
+  interface = routeros_interface_bonding.kube03.name
+  comment   = routeros_interface_bonding.kube03.comment
+  pvid      = "1"
+}
+resource "routeros_interface_bridge_port" "pve" {
+  bridge    = routeros_interface_bridge.bridge.name
+  interface = routeros_interface_bonding.pve.name
+  comment   = routeros_interface_bonding.pve.comment
+  pvid      = "1"
+}
+resource "routeros_interface_bridge_port" "crs326" {
+  bridge    = routeros_interface_bridge.bridge.name
+  interface = routeros_interface_bonding.crs326.name
+  comment   = routeros_interface_bonding.crs326.comment
+  pvid      = "1"
+}
+resource "routeros_interface_bridge_port" "mirk-desktop" {
+  bridge    = routeros_interface_bridge.bridge.name
+  interface = routeros_interface_ethernet.mirk-desktop.name
+  comment   = routeros_interface_ethernet.mirk-desktop.comment
+  pvid      = "1"
+}
+
