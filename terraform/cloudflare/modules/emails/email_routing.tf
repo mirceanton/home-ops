86aa8cd6466aba897b7043a620ac1d64033fe1fb
Mircea-Pavel ANTON <contact@mirceanton.com>
2026-01-10 20:51:55 +0200

    feat!: ah yes... a fresh start

--- terraform/cloudflare/modules/emails/email_routing.tf
+++
@@ -1,5 +0,0 @@
-resource "cloudflare_email_routing_settings" "email_routing_enable" {
-  zone_id     = var.zone_id
-  enabled     = "true"
-  skip_wizard = true
-}
--- terraform/cloudflare/modules/emails/target_address.tf
+++
@@ -1,6 +0,0 @@
-resource "cloudflare_email_routing_address" "gmail" {
-  for_each = var.target_addresses
-
-  account_id = var.account_id
-  email      = each.value
-}
